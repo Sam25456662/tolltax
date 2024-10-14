3c6e9daaff25930160e9109d86a36a499581f0fa
Samriddha Laskar <samriddhalaskar6@gmail.com>
2024-10-14 14:42:04 +0530

    "sam chnages"

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,12 +21,12 @@ $location = Location::count();
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0 text-dark">Admin Dashboard</h1>
+                        <h1 class="m-0 text-dark">admin Dashboard</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">AdminDashboard</li>
+                            <li class="breadcrumb-item active">Admin Dashboard</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
